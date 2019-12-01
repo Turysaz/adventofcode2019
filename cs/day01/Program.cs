936e0dbe42bcc01b213006d90280a71acdab6a8f
Turysaz <turysaz@posteo.org>
2019-12-01 15:29:43 +0100

    Day 01: C#

---
+++ cs/day01/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace day01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Fuel(int mass, bool recursive = false)
+            {
+                var f = (int)(Math.Floor(mass/3.0)) - 2;
+
+                if (recursive == false)
+                {
+                    return f;
+                }
+                
+                return f < 0 ? 0 : f + Fuel(f, true);
+            }
+
+            var modules = File.ReadAllLines("../../inputs/day01.txt")
+                .Select(l => Int32.Parse(l));
+
+            // Part 1
+            Console.WriteLine(modules.Select(module => Fuel(module)).Sum());
+
+            // Part 2
+            Console.WriteLine(modules.Select(
+                    module => Fuel(module, true))
+                .Sum());
+        }
+    }
+}
